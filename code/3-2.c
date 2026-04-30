2922f3e98b1d901ea0b9704245fcce20c842ed69
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-04-30 22:50:18 +0900

    3-2.c

---
+++ code/3-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  for (int i = 1; i < argc - 1; i++) {
+        for (int j = 1; j < argc - i; j++) {
+            if (strcmp(argv[j], argv[j + 1]) > 0) {
+                char *temp = argv[j];
+                argv[j] = argv[j + 1];
+                argv[j + 1] = temp;
+            }
+        }
+    }
+
+  for (int i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
+  return 0;
+}
